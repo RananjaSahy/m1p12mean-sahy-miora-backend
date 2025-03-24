57e151c13df7fb24d368b76ac15fcf30c8f7caf9
RananjaSahy <sahyrananja2003@gmail.com>
2025-03-24 10:04:12 +0300

    Type vehicule et mecanicien

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const generateToken = (user) => {
     return jwt.sign(
-        { id: user._id, role: user.role },
+        { id: user._id, role: user.role, email: user.email },
         process.env.JWT_SECRET,
         { expiresIn: process.env.JWT_EXPIRES_IN }
     );
--- routes/utilisateurs.routes.js
+++ routes/utilisateurs.routes.js
@@ -24,4 +24,28 @@ router.get('/mesvehicules', authMiddleware(['client']), async(req,res) => {
     }
 });
 
+router.get("/mecaniciens", authMiddleware(['manager']), async(req, res) => {
+    try{
+        const { page = 1, limit = 10, search = "" } = req.query;
+        const searchQuery = search
+        ? {
+              $or: [
+                  { nom: { $regex: search, $options: "i" } },
+                  { prenom: { $regex: search, $options: "i" } },
+                  { email: { $regex: search, $options: "i" } },
+              ],
+          }
+        : {};
+        const totalMecaniciens = await Utilisateur.countDocuments({ role: "mecanicien", ...searchQuery });
+        const mecaniciens = await Utilisateur.find({role: "mecanicien", ...searchQuery}).select('-mdp')
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit))
+        .sort({ createdAt: -1 });
+        res.status(200).json({mecaniciens, total: totalMecaniciens});
+    }catch(error){
+        res.status(500).json({message : error.message});
+        console.log(error.message);
+    }
+});
+
 module.exports = router;
